315b4ca7cf8d03522c5ec860df6efb65582087fe
bogdan.virtosu <virtosu.bogdan@gmail.com>
2016-04-07 09:18:38 +0300

    LUN-2796: Add tests for rendering context rework.

--- cmsplugin_filer_image/tests/tests.py
+++ cmsplugin_filer_image/tests/tests.py
@@ -1,15 +1,19 @@
-from django.test import TestCase
 import os
+
+from django.test import TestCase
 from django.core.files import File as DjangoFile
+
 from filer.models.imagemodels import Image
 from filer.models.filemodels import File
-from cmsplugin_filer_image.models import ThumbnailOption
+from filer.tests.helpers import create_superuser, create_image
+
+from cmsplugin_filer_image.models import ThumbnailOption, FilerImage
+from cmsplugin_filer_image.cms_plugins import FilerImagePlugin
 
 
 class ThumbnailOptionsTest(TestCase):
 
     def setUp(self):
-        from filer.tests.helpers import create_superuser, create_image
         self.superuser = create_superuser()
         self.img = create_image()
         self.filename = os.path.join(os.path.dirname(__file__),
@@ -73,3 +77,91 @@ class ThumbnailOptionsTest(TestCase):
                                  ['Original -- 1024 x 576', 'Large -- 616 x XXX',
                                   'Medium -- 320 x XXX', 'Small -- 180 x XXX'],
                                  lambda o: str(o))
+
+
+class RenderingTest(TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        self.superuser = create_superuser()
+        self.img = create_image()
+        self.filename = os.path.join(os.path.dirname(__file__), 'test.jpg')
+        self.img.save(self.filename, 'JPEG')
+        self.image = self.create_filer_image()
+        self.image.save()
+        self.filer_image = FilerImage.objects.create(image=self.image)
+
+    def create_filer_image(self):
+        file_obj = DjangoFile(open(self.filename), name='test.jpg')
+        image = Image.objects.create(owner=self.superuser,
+                                     original_filename='test.jpg',
+                                     file=file_obj)
+        return image
+
+    def test_create_context(self):
+        plugin = FilerImagePlugin()
+        actual_context = plugin.render({}, self.filer_image, None)
+        actual_context.pop('rnd')
+        expected_context = {
+            'caption': '',
+            'container_attributes': '',
+            'container_classes': '',
+            'container_style': 'width:800px;',
+            'credit': '',
+            'details_style': 'width:800px;',
+            'image_height': 600,
+            'image_url': self.image.url,
+            'image_width': 800,
+            'img_alt': 'test.jpg',
+            'link': '',
+            'link_target': '_self',
+            'overlay_link': ''
+        }
+        self.assertEqual(expected_context, actual_context)
+
+    def test_create_context_all_options(self):
+        self.filer_image.alt_text = "alt value"
+        self.filer_image.caption_text = "caption value"
+        self.filer_image.credit_text = "credit value"
+        self.filer_image.show_caption = True
+        self.filer_image.show_credit = True
+        self.filer_image.link_options = 2
+        self.filer_image.free_link = "link address"
+        self.filer_image.target_blank = True
+        self.filer_image.width = 201
+        self.filer_image.height = 300
+        self.filer_image.maintain_aspect_ratio = True
+        self.filer_image.left_pace = 1
+        self.filer_image.right_pace = 2
+        self.filer_image.top_pace = 3
+        self.filer_image.bottom_pace = 4
+        self.filer_image.border = 5
+        self.filer_image.enable_event_tracking = True
+        self.filer_image.event_category = "event category"
+        self.filer_image.event_action = "event action"
+        self.filer_image.event_label = "event label"
+
+        plugin = FilerImagePlugin()
+        actual_context = plugin.render({}, self.filer_image, None)
+        actual_context.pop('rnd')
+        image_url = actual_context.pop('image_url')
+        self.assertTrue("201x300" in image_url, "Image is not resized!")
+        expected_context = {
+            'caption': 'caption value',
+            'container_attributes': '',
+            'container_classes': ' has-caption has-credit',
+            'container_style': 'border: 5px solid black;width:201px;',
+            'credit': 'credit value',
+            'details_style': 'width:201px;',
+            'event_tracking': {
+                'action': 'event action',
+                'category': 'event category',
+                'label': 'event label',
+            },
+            'image_height': 151,
+            'image_width': 201,
+            'img_alt': 'alt value',
+            'link': 'link address',
+            'link_target': '_blank',
+            'overlay_link': ''}
+        self.assertEqual(expected_context, actual_context)
